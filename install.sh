06c5971ea53fe33646ee0e9dc1f778f15994ebf1
qpq-14-64 <qiuworker@163.com>
2018-12-03 16:05:08 +0800

    add install ecs shell

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt-get install nmap --yes
+apt-get install traceroute --yes
+apt-get install git --yes
+apt-get install git-core --yes
+apt-get install screen --yes
+apt-get install samba samba-common smbclient --yes
+apt-get install jq --yes
+apt-get install curl --yes
+apt-get install perl python git --yes
+apt-get install vsftpd --yes
+apt-get install nmap --yes
+apt-get install traceroute --yes
+apt-get install git --yes
+apt-get install git-core --yes
+apt-get install screen --yes
+apt-get install samba samba-common smbclient --yes
+apt-get install jq --yes
+apt-get install curl --yes
+apt-get install perl python git --yes
+apt-get install vsftpd --yes
+apt-get install polipo --yes
+apt-get install socat --yes
+apt-get install nginx --yes
+
