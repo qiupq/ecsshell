3962e310e667f863820ee25302c42f830f311e32
qpq-14-64 <qiuworker@163.com>
2018-12-05 11:25:17 +0800

    add msleep in run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@
 touch ~/v2ray.log
 apt-get update 
 apt-get install git git-core unzip nginx screen -y
-wget https://codeload.github.com/qiupq/ecsshell/zip/master
-unzip master -d ~
+service nginx start
+sleep 1
+wget https://codeload.github.com/qiupq/ecsshell/zip/master -O v2ray.zip
+unzip v2ray.zip -d ~
 cp -rf  ~/ecsshell-master/nginx/default-v1.4.6  /etc/nginx/sites-enabled/default
 service nginx restart
+sleep 1
 mkdir -p /etc/v2ray/
 cp -rf ~/ecsshell-master/nginx/v2ray.* /etc/v2ray/
 ~/ecsshell-master/v2ray/v2ray -config ~/ecsshell-master/v2ray/service_ws-tls-nginx.json  &
