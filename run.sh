73368a07c020b868de333d99d3b8456d943e6f4a
qpq-14-64 <qiuworker@163.com>
2018-12-05 11:18:03 +0800

    add ssl certification;add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-export https_proxy=http://localhost:1090
-export http_proxy=http://localhost:1090
+#export https_proxy=http://localhost:1090
+#export http_proxy=http://localhost:1090
+touch ~/v2ray.log
+apt-get update 
+apt-get install git git-core unzip nginx screen -y
+wget https://codeload.github.com/qiupq/ecsshell/zip/master
+unzip master -d ~
+cp -rf  ~/ecsshell-master/nginx/default-v1.4.6  /etc/nginx/sites-enabled/default
+service nginx restart
+mkdir -p /etc/v2ray/
+cp -rf ~/ecsshell-master/nginx/v2ray.* /etc/v2ray/
+~/ecsshell-master/v2ray/v2ray -config ~/ecsshell-master/v2ray/service_ws-tls-nginx.json  &
+
+date 2>&1 |tee ~/v2ray.log 
