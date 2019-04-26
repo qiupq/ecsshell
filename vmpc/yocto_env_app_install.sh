47ad6b021d08c95e51c880ba33cb0b069e11360e
qpq-14-64 <qiuworker@163.com>
2019-04-26 11:28:17 +0800

    update yocto app bash shell

--- vmpc/yocto_env_app_install.sh
+++ vmpc/yocto_env_app_install.sh
@@ -30,4 +30,9 @@ apt-get install u-boot-tools --yes
 apt-get install libsdl1.2-dev xterm sed cvs subversion coreutils texi2html \
 docbook-utils python-pysqlite2 help2man make gcc g++ desktop-file-utils \
 libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff curl lzop asciidoc --yes
+# RF:https://wiki.phytec.com/pages/viewpage.action?pageId=78807233
+apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib \
+build-essential chrpath socat libsdl1.2-dev xterm g++-multilib \
+cpio python python3 python3-pip python3-pexpect \
+xz-utils debianutils iputils-ping  --yes
 echo "all qq app done "
