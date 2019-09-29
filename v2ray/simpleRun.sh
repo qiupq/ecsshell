972984889986e4ae3f9552f5fe1037e808eeda92
qq <qiuworker@126.com>
2019-09-29 10:16:28 +0800

    add simpleRun.sh

---
+++ v2ray/simpleRun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Asia/Shanghai" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+ ps -xua |grep v2ray |grep config |tr -s [:space:]|cut -f 2 -d " "|xargs -i kill -9 {}
+sleep 1
+./4.18.0/v2ray -config ./4.18.0/vpoint_vmess_freedom.json &
+
