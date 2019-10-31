1cfb9913f786d88cbc0df129f868d6acc8b57aaf
qq <qiuworker@126.com>
2019-10-31 16:41:02 +0800

    update android9 to ubuntu 16.04

--- vmpc/android9.sh
+++ vmpc/android9.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-mkdir -p /home/qq/
 cd /home/qq
 
 #set evironment
+if [ -f .gitconfig ]; then    
+	echo "check done  --- .gitconfig" 
+	
+else                                                                                   
+	echo "create .gitconfig"  
+	
 git config --global user.email "qiuworker@126.com"
 git config --global user.name "qq"
+fi 
+
+if [ -f .ssh/id_rsa.pub ]; then    
+	echo "check done  --- .ssh/id_rsa.pub" 
+	
+else                                                                                   
+	echo "create .ssh/id_rsa.pub"  
+	
 ssh-keygen  -t rsa -P '' -f ~/.ssh/id_rsa
+fi 
 function get_android_code
 {
 	echo "get android code"
@@ -16,19 +30,29 @@ function get_android_code
 #repo init -u  git://github.com/AOSiP/platform_manifest.git -b pie
 	repo sync -j`nproc` --no-repo-verify --force-sync --no-tags --no-clone-bundle
 #repo sync --no-tags --no-clone-bundle --no-repo-verify
+    repo start android-pie --all
 }
 function build_android
 {
 	echo "build android"
 	cd /home/qq/android_pie
+
 	source build/envsetup.sh
-	
+# Preparing a Compiler Cache
+    export USE_CCACHE=1
+	prebuilts/misc/linux-x86/ccache/ccache -M 15G
+
 	lunch aosip_device-userdebug
+	#make -j8 2>&1 |tee -a androidbuild.log
     time mka kronic
-	
+#clean up the result of previous builds	
+#make clobber
 }
 export LANG=en_US.UTF-8
 
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4g"
+
+export ANDROID_JACK_VM_ARGS="-Xmx4g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 
 if [ $1 == "test" ]
 then
@@ -36,5 +60,5 @@ then
 	get_android_code
 else
 	echo "phyteclinux"
-	build_android
+	#build_android
 fi
--- vmpc/android_env_install.sh
+++ vmpc/android_env_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "install start->>> "
-add-apt-repository ppa:openjdk-r/ppa
+echo "ubuntu 16.04 install start->>> "
+add-apt-repository ppa:openjdk-r/ppa  <<EOF
+
+EOF
+
 apt-get update 
 apt-get install openjdk-8-jdk --yes
 
@@ -11,9 +14,11 @@ export JAVA_HOME
 export PATH" >> /etc/profile
 apt-get install git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses5-dev:i386 x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386  libgl1-mesa-dev g++-multilib tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386 dpkg-dev ccache  --yes
 apt-get install curl ccache --yes
+apt-get install android-tools-adb bc bison build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev --yes
 ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
-curl http://android.git.kernel.org/repo >/usr/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo >/usr/bin/repo
 chmod a+x /usr/bin/repo
+chown qq:qq /usr/bin/repo
 
 echo "all qq app done "
