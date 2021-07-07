0f5112ed78bb2a6d24bc109460d35e2c1654d0f3
qiupq <qiupq@xm-ryd.com>
2021-07-07 09:24:54 +0800

    update for simple install

---
+++ v2ray/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#set -x
+echo "install v2ray app ..."
+CWD=$(pwd)
+INSTALLDIR=/usr/bin/v2ray
+mkdir -p $INSTALLDIR
+cp -apf $CWD/4.31.0/* $INSTALLDIR
+timedatectl set-timezone  Asia/Shanghai
+#ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+CONFIGDIR=/etc/v2ray
+mkdir -p $CONFIGDIR
+cp $CWD/service.json $CONFIGDIR
+cp $CWD/client.json  $CONFIGDIR
+
+SYSTEMDDIR=/etc/systemd/system/
+cp $CWD/4.31.0/systemd/system/v2ray.service $SYSTEMDDIR
+cp $CWD/4.31.0/systemd/system/v2ray_server.service $SYSTEMDDIR
+systemctl daemon-reload
+systemctl enable v2ray.service
+systemctl enable v2ray_server.service
+
+#need files when running
+mkdir -p /var/log/v2ray/
+touch -m /var/log/v2ray/access.log
+touch -m /var/log/v2ray/error.log 
+
+echo "to the end."
+exit 0
--- v2ray/simpleRun.sh
+++ v2ray/simpleRun.sh
@@ -3,5 +3,5 @@ echo "Asia/Shanghai" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
  ps -xua |grep v2ray |grep config |tr -s [:space:]|cut -f 2 -d " "|xargs -i kill -9 {}
 sleep 1
-./4.18.0/v2ray -config ./4.18.0/vpoint_vmess_freedom.json &
+./4.31.0/v2ray -config ./4.31.0/freedom.json &
 
