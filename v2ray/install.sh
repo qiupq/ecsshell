85cd1960e48572f9a3207c2f6c124883941ff44b
qiupq <qiupq@xm-ryd.com>
2023-11-16 17:59:15 +0800

    update v2ray config

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -4,7 +4,7 @@ echo "install v2ray app ..."
 CWD=/root/ecsshell-master/v2ray
 INSTALLDIR=/usr/bin/v2ray
 mkdir -p $INSTALLDIR
-cp -apf $CWD/4.31.0/* $INSTALLDIR
+cp -apf $CWD/5.3.0/* $INSTALLDIR
 timedatectl set-timezone  Asia/Shanghai
 #ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
@@ -14,8 +14,8 @@ cp $CWD/service.json $CONFIGDIR
 cp $CWD/client.json  $CONFIGDIR
 
 SYSTEMDDIR=/etc/systemd/system/
-cp $CWD/4.31.0/systemd/system/v2ray.service $SYSTEMDDIR
-cp $CWD/4.31.0/systemd/system/v2ray_server.service $SYSTEMDDIR
+cp $CWD/5.3.0/systemd/system/v2ray.service $SYSTEMDDIR
+cp $CWD/5.3.0/systemd/system/v2ray_server.service $SYSTEMDDIR
 systemctl daemon-reload
 #systemctl enable v2ray.service
 systemctl enable v2ray_server.service
