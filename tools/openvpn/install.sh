0c95dbb8f938674827aebc42209a971bb2a5064a
qiupq <qiupq@xm-ryd.com>
2023-11-24 12:01:51 +0800

    update config

--- tools/openvpn/install.sh
+++ tools/openvpn/install.sh
@@ -16,7 +16,7 @@ mkdir -p $CONFIGDIR
 cp $CWD/*.ovpn $CONFIGDIR
 
 SYSTEMDDIR=/etc/systemd/system/
-cp $CWD/systemd/system/openvpns.service $SYSTEMDDIR
+cp $CWD/openvpns.service $SYSTEMDDIR
 #cp $CWD/systemd/system/openvpns.service $SYSTEMDDIR
 systemctl daemon-reload
 
@@ -24,9 +24,20 @@ systemctl daemon-reload
 #systemctl enable openvpn_client.service
 #systemctl restart openvpn_client.service
 
-echo "1" > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -j MASQUERADE
+CLIENT_INTF="tun0"
+SERVER_INTF="enp1s0"
 
+iptables-legacy -t nat -F
+echo "1"
+iptables-legacy -A FORWARD -i $SERVER_INTF -o $CLIENT_INTF -m state --state ESTABLISHED,RELATED -j ACCEPT
+echo "2"
+iptables-legacy -A FORWARD -i $CLIENT_INTF -o $SERVER_INTF -j ACCEPT
+echo "3"
+iptables-legacy -t nat -A POSTROUTING -o $SERVER_INTF -j MASQUERADE
+
+echo "1" > /proc/sys/net/ipv4/ip_forward
+#iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -j MASQUERADE
+#iptables-legacy -t nat -A POSTROUTING -j MASQUERADE
 #service run
 systemctl enable openvpns.service
 systemctl restart openvpns.service
