022178e5b081abaa04b6bed4fec379cfd1696c2f
qiupq <qiupq@xm-ryd.com>
2025-06-13 15:30:40 +0800

    fixed systemd run error

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -2,9 +2,12 @@
 #set -x
 echo "install v2ray app ..."
 CWD=/root/ecsshell-master/v2ray
-INSTALLDIR=/usr/bin/v2ray
+INSTALLDIR=/usr/v2ray
+rm -rf /usr/bin/v2ray
+rm -rf $INSTALLDIR/
 mkdir -p $INSTALLDIR
 cp -apf $CWD/5.32.0/* $INSTALLDIR
+ln -sf  $INSTALLDIR/v2ray /usr/bin/v2ray
 timedatectl set-timezone  Asia/Shanghai
 #ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
@@ -17,13 +20,12 @@ SYSTEMDDIR=/etc/systemd/system/
 cp $CWD/5.32.0/systemd/system/v2ray.service $SYSTEMDDIR
 cp $CWD/5.32.0/systemd/system/v2ray_server.service $SYSTEMDDIR
 systemctl daemon-reload
-#systemctl enable v2ray.service
-systemctl enable v2ray_server.service
 
 #need files when running
 mkdir -p /var/log/v2ray/
 touch -m /var/log/v2ray/access.log
 touch -m /var/log/v2ray/error.log 
+systemctl enable v2ray_server.service
 systemctl restart v2ray_server.service
 
 echo "to the end."
